a7fd2f98877434b39545afca5bf74c0922ca42a8
tca85 <thiago.alves.85@gmail.com>
2015-12-09 15:07:25 -0200

    2ª versão modificada para rodar no java 4 (1.4.2)

--- src/br/com/ache/lims/ReadExcel.java
+++ src/br/com/ache/lims/ReadExcel.java
@@ -2,153 +2,107 @@ package br.com.ache.lims;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
+
 /**
- * A dirty simple program that reads an Excel file
- * from www.codejava.net
+ * Converter arquivo excel em texto (.txt) para ser utilizado via comando na SM69 do SAP
+ *  
+ * obs: deve ser compilado na versão 1.4.2 do Java
  * 
- * @author tca85
+ * 09.12.2015
+ * 
+ * @author Thiago Cordeiro Alves / Uderson Fermino
  *
  */
 public class ReadExcel {
 	public static void main(String[] args) {
-		new ReadExcel().buscarArquivos();
-	}
-
-	/**
-	 * busca arquivos...
-	 */
-	private void buscarArquivos() {
-		List<Properties> listaProperties = new ArrayList<Properties>();
-
-		try {
-			buscarArquivoProperties(listaProperties, "resources/excel2txt.properties");
-
-			converterExcelTXT(listaProperties);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	/**
-	 * 
-	 * @param listaProperties
-	 * @param nomeArquivo
-	 * @throws IOException
-	 * @throws FileNotFoundException
-	 */
-	private void buscarArquivoProperties(List<Properties> listaProperties, String nomeArquivo) throws IOException, FileNotFoundException {
-		Properties propertiesFile = new Properties();
-		InputStream ipt1 = getClass().getClassLoader().getResourceAsStream(nomeArquivo);
-
-		if (ipt1 != null) {
-			propertiesFile.load(ipt1);
-			listaProperties.add(propertiesFile);
-			ipt1.close();
-		} else {
-			throw new FileNotFoundException("arquivo de propriedades " + nomeArquivo + " não encontrado");
-		}
-	}	
-	
-	/**
-	 * Converte arquivo Excel para TXT
-	 */
-	private void converterExcelTXT(List<Properties> listaProperties) {
-		File dirOrigem;
-		File dirDestino;
-		FileInputStream inputStream;
+//		File dirOrigem = new File("/opt/conversor_lims/origem/");
+//		File dirDestino = new File("/opt/conversor_lims/destino/");
+		
+		File dirOrigem = new File("c:/temp/excel/origem/");
+		File dirDestino = new File("c:/temp/excel/destino/");
 		
 		String separator = System.getProperty("file.separator");
 
-		for (Properties prop : listaProperties) {
-			try {
-				System.out.println("Verificando se existem arquivos para serem convertidos...");
+		try {
+			Iterator listaArquivosXLS = FileUtils.iterateFiles(dirOrigem, null, false);
+			
+			if (!listaArquivosXLS.hasNext()) {
+				System.out.println("Não existem arquivos XLS para serem convertidos em TXT");
+			}			
 
-				dirOrigem = new File(prop.getProperty("dir_origem"));
-				dirDestino = new File(prop.getProperty("dir_destino"));
+			while (listaArquivosXLS.hasNext()) {
+				File arquivo = ((File) listaArquivosXLS.next());
 
-				for (File arquivo : dirOrigem.listFiles()) {
-					if (arquivo.getName().toUpperCase().endsWith(".XLS")) {
-						System.out.println("Lendo aquivo excel");
+				if (arquivo.getName().toUpperCase().endsWith(".XLS")) {
+					String value = "";
 
-						inputStream = new FileInputStream(arquivo);
-						Workbook workbook = new HSSFWorkbook(inputStream);
+					System.out.println("Lendo aquivo XLS...");
 
-						Sheet firstSheet = workbook.getSheetAt(Integer.parseInt(prop.getProperty("sheet")));
-						Iterator<Row> iterator = firstSheet.iterator();
+					FileInputStream inputStream = new FileInputStream(arquivo);
+					Workbook workbook = new HSSFWorkbook(inputStream);
 
-						String value = "";
+					Sheet firstSheet = workbook.getSheetAt(2);
 
-						while (iterator.hasNext()) {
-							Row nextRow = iterator.next();
+					for (int i = 0; i <= firstSheet.getLastRowNum(); i++) {
+						Row nextRow = firstSheet.getRow(i);
 
-							if (nextRow.getRowNum() > Integer.parseInt(prop.getProperty("sheet_row"))) {
-								Iterator<Cell> cellIterator = nextRow.cellIterator();
-								System.out.println("Lendo nova excel do excel");
+						if (nextRow.getRowNum() >= 10) {
+							for (int j = 0; j <= nextRow.getLastCellNum(); j++) {								
+								Cell cell = nextRow.getCell(j);
 
-								while (cellIterator.hasNext()) {
-									Cell cell = cellIterator.next();
-									if (cell != null && cell.getCellType() != Cell.CELL_TYPE_BLANK) {
-										switch (cell.getCellType()) {
-										case Cell.CELL_TYPE_STRING:
-											value = value + cell.getStringCellValue() + ";";
-											break;
-										case Cell.CELL_TYPE_BOOLEAN:
-											value = value + String.valueOf(cell.getBooleanCellValue()) + ";";
-											break;
-										case Cell.CELL_TYPE_NUMERIC:
-											value = value + String.valueOf(cell.getNumericCellValue()) + ";";
-											break;
-										}
+								if (cell != null && cell.getCellType() != Cell.CELL_TYPE_BLANK) {
+									switch (cell.getCellType()) {
+									case Cell.CELL_TYPE_STRING:
+										value = value + cell.getStringCellValue() + ";";
+										break;
+									case Cell.CELL_TYPE_BOOLEAN:
+										value = value + String.valueOf(cell.getBooleanCellValue()) + ";";
+										break;
+									case Cell.CELL_TYPE_NUMERIC:
+										value = value + String.valueOf(cell.getNumericCellValue()) + ";";
+										break;
 									}
 								}
-								value = value + "\n";
 							}
+							value = value + "\n";
 						}
+					}
 
-						System.out.println("Gravando aquivo convertido");
+					File file = new File(dirDestino.getAbsolutePath() + separator + arquivo.getName().replaceAll(".xls", ".txt"));
+					FileWriter fileWriter = null;
 
-						File file = new File(dirDestino.getAbsolutePath() + separator + arquivo.getName().replaceAll(".xls", ".txt"));
-						FileWriter fileWriter = null;
-
+					try {
+						fileWriter = new FileWriter(file);
+						fileWriter.write(value.replaceAll("\n", System.getProperty("line.separator")));
+						fileWriter.close();
+						System.out.println("Arquivo convertido com sucesso");
+					} catch (IOException ex) {
+						System.out.println(ex.getMessage());
+					} finally {
 						try {
-							fileWriter = new FileWriter(file);
-							fileWriter.write(value.replaceAll("\n", System.getProperty("line.separator")));
 							fileWriter.close();
-							System.out.println("Gravando aquivo convertido em txt");
 						} catch (IOException ex) {
 							System.out.println(ex.getMessage());
-						} finally {
-							try {
-								fileWriter.close();
-							} catch (IOException ex) {
-								System.out.println(ex.getMessage());
-							}
 						}
-						arquivo.delete();
-						workbook.close();
-						inputStream.close();
 					}
+					arquivo.delete();
+					inputStream.close();
 				}
-			} catch (Exception e) {
-				System.out.println(e);
-				e.printStackTrace();
 			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
